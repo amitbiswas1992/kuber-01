e3c0a26d6182ac1ba984abc16a71f8cd9b04580c
Amit  Biswas <amitbiswas1992@github.com>
2025-03-19 20:02:52 -0400

    Added new template

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ app.use(express.json());
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Basic API routes
-app.get('/api', (req, res) => {
-    res.json({
-        message: 'Welcome to the Node.js Docker App!'
-    });
-});
-
 // Health check endpoint
 app.get('/health', (req, res) => {
     res.json({
@@ -26,24 +19,139 @@ app.get('/health', (req, res) => {
     });
 });
 
-// Route for the home page
+// Main routes for different HTML versions
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Route for about page
+// About routes
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
-// Route for contact page
+app.get('/02-about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_about.html'));
+});
+
+app.get('/03-about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_about.html'));
+});
+
+app.get('/06-about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '06_about.html'));
+});
+
+app.get('/07-about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '07_about.html'));
+});
+
+// Contact routes
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
+app.get('/02-contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_contact.html'));
+});
+
+app.get('/03-contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_contact.html'));
+});
+
+app.get('/04-contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '04_contact.html'));
+});
+
+app.get('/06-contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '06_contact.html'));
+});
+
+app.get('/07-contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '07_contact.html'));
+});
+
+// Blog routes
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog.html'));
+});
+
+app.get('/02-blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_blog.html'));
+});
+
+app.get('/03-blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_blog.html'));
+});
+
+// Blog Single routes
+app.get('/blog-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog-single.html'));
+});
+
+app.get('/02-blog-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_blog-single.html'));
+});
+
+app.get('/03-blog-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_blog-single.html'));
+});
+
+app.get('/04-blog-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '04_blog-single.html'));
+});
+
+// Services routes
+app.get('/services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'services.html'));
+});
+
+app.get('/02-services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_services.html'));
+});
+
+app.get('/03-services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_services.html'));
+});
+
+app.get('/06-services', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '06_services.html'));
+});
+
+// Works routes
+app.get('/works', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'works.html'));
+});
+
+app.get('/02-works', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_works.html'));
+});
+
+app.get('/03-works', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_works.html'));
+});
+
+// Work Single routes
+app.get('/work-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'work-single.html'));
+});
+
+app.get('/02-work-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '02_work-single.html'));
+});
+
+app.get('/03-work-single', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '03_work-single.html'));
+});
+
+// PHP handler route
+app.post('/handler.php', (req, res) => {
+    // Handle form submissions here
+    res.json({ status: 'success', message: 'Form submitted successfully' });
+});
+
 // Handle 404 - Page Not Found
 app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+    res.status(404).send('Page Not Found');
 });
 
 // Start the server
